53d7960df69b6cac1e20492379e7f1ae65aa2989
mikezielony <mike.zielony@gmail.com>
2021-04-24 08:01:59 +0200

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDmXqXDqnQk8arrmM-6sK0z2dukseCqvjc",
+    authDomain: "thegame-6f5eb.firebaseapp.com",
+    databaseURL: "https://thegame-6f5eb-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "thegame-6f5eb",
+    storageBucket: "thegame-6f5eb.appspot.com",
+    messagingSenderId: "117162735941",
+    appId: "1:117162735941:web:600d340aafc2783c83262c",
+    measurementId: "G-QRJJ5LWCY4"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDmXqXDqnQk8arrmM-6sK0z2dukseCqvjc",
+    authDomain: "thegame-6f5eb.firebaseapp.com",
+    databaseURL: "https://thegame-6f5eb-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "thegame-6f5eb",
+    storageBucket: "thegame-6f5eb.appspot.com",
+    messagingSenderId: "117162735941",
+    appId: "1:117162735941:web:600d340aafc2783c83262c",
+    measurementId: "G-QRJJ5LWCY4"
+  }
 };
 
 /*
